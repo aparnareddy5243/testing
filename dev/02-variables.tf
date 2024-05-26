346b2bc5213aa96572b90b72759f466ccebd782d
Aparna <apputmreddy@gmail.com>
2024-05-26 15:32:37 +0100

    ssfffqs

--- dev/02-variables.tf
+++ dev/02-variables.tf
@@ -25,6 +25,7 @@ variable "environment" {
 }
 
 
+
 # AKS Input Variables
 
 # SSH Public Key for Linux VMs
@@ -32,6 +33,22 @@ variable "ssh_public_key" {
   default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
+
+# Windows Admin Username for k8s worker nodes
+variable "windows_admin_username" {
+  type = string
+  default = "azureuser"
+  description = "This variable defines the Windows admin username k8s Worker nodes"  
+}
+
+# Windows Admin Password for k8s worker nodes
+variable "windows_admin_password" {
+  type = string
+  default = "StackSimplify@102"  # Updated June 2023
+  description = "This variable defines the Windows admin password k8s Worker nodes"  
+}
+
+
 
 
 
--- prod/02-variables.tf
+++ prod/02-variables.tf
@@ -25,13 +25,30 @@ variable "environment" {
 }
 
 
+
 # AKS Input Variables
 
-# SSH Public Key for Linu
+# SSH Public Key for Linux VMs
 variable "ssh_public_key" {
   default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
+
+# Windows Admin Username for k8s worker nodes
+variable "windows_admin_username" {
+  type = string
+  default = "azureuser"
+  description = "This variable defines the Windows admin username k8s Worker nodes"  
+}
+
+# Windows Admin Password for k8s worker nodes
+variable "windows_admin_password" {
+  type = string
+  default = "StackSimplify@102"  # Updated June 2023
+  description = "This variable defines the Windows admin password k8s Worker nodes"  
+}
+
+
 
 
 
